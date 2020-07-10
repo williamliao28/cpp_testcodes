06897d864b0ac0d3744908c27a462eeee73ca997
William Liao <r05246012@ntu.edu.tw>
2020-07-11 00:01:06 +0800

    update codes

--- src/c/logical.c
+++ src/c/logical.c
@@ -4,9 +4,9 @@
 int main(int argc, char *argv[])
 {
     int a=30,b=10,c=20;
-    printf(" %d \n", a>b && a>c); 	/*  1:true	*/
-    printf(" %d \n", a>b || a<c);   /*  1:true 	*/
-    printf(" %d \n", !(a>b));      /*  0:false 	*/
+    printf("(a>b && a>c): %d \n", a>b && a>c); 	/*  1:true	*/
+    printf("(a>b || a<c): %d \n", a>b || a<c);   /*  1:true 	*/
+    printf("!(a>b): %d \n", !(a>b));      /*  0:false 	*/
 	
     return 0;
 }
--- src/c/relation.c
+++ src/c/relation.c
@@ -4,10 +4,10 @@
 int main(int argc, char **argv)
 {
     int a=10, b=3;
-	printf(" %d \n", a<b); 	    /*  0:false 	*/
-	printf(" %d \n", a>b);  	/*  1:true  	*/
-	printf(" %d \n", a==b); 	/*  0:false  	*/
-	printf(" %d \n", a!=b); 	/*  1:true  	*/
+	printf("a<b:  %d \n", a<b); 	/*  0:false 	*/
+	printf("a>b:  %d \n", a>b);  	/*  1:true  	*/
+	printf("a==b: %d \n", a==b); 	/*  0:false  	*/
+	printf("a!=b: %d \n", a!=b); 	/*  1:true  	*/
 	
 	return 0;
 }
