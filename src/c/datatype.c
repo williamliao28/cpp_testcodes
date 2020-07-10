0c88849d21d14cb7e6c6b48e61a115cb5e19205e
William Liao <r05246012@ntu.edu.tw>
2020-07-10 18:04:56 +0800

    fix errors to avoid warnings

--- src/c/datatype.c
+++ src/c/datatype.c
@@ -13,32 +13,32 @@ int main(int argc, char **argv){
     a1 = 234;
     a2 = -234;
     // Displaying integers using different placeholders
-    printf('%d\n',a1);
-    printf('%4d\n',a1);
-    printf('%5d\n',a1);
-    printf('%6d\n',a1);
+    printf("%d\n",a1);
+    printf("%4d\n",a1);
+    printf("%5d\n",a1);
+    printf("%6d\n",a1);
 
-    printf('\n');
+    printf("\n");
 
-    printf('%4d\n',a2);
-    printf('%5d\n',a2);
-    printf('%6d\n',a2);
-    printf('%2d\n',a2);
+    printf("%4d\n",a2);
+    printf("%5d\n",a2);
+    printf("%6d\n",a2);
+    printf("%2d\n",a2);
 
-    printf('\n');
+    printf("\n");
 
     // Displaying floating point numbers
     b = sqrt(2);
     c = sqrt(2);
 
-    printf('%.3f\t%.3f\n',b,c);
-    printf('%.4f\t%.4f\n',b,c);
-    printf('%.5f\t%.5f\n',b,c);
+    printf("%.3f\t%.3f\n",b,c);
+    printf("%.4f\t%.4f\n",b,c);
+    printf("%.5f\t%.5f\n",b,c);
 
-    printf('\n');
+    printf("\n");
 
-    printf('%f\n',b);
-    printf('%lf\n',c);
+    printf("%f\n",b);
+    printf("%lf\n",c);
 
     return 0;
 }
